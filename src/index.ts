f637c6544cc9142884857c1d38f0d025f0617066
Adrian Burlacu <adrian.burlacu@live.com>
2022-10-14 11:39:53 +0300

    Add the main function to the browser export.

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ async function main() {
   const appDataSource = await getAppDataSource();
   await appDataSource.initialize();
 
-  console.log("Inserting a new user into the database...")
-  const user = new User()
-  user.firstName = "Timber"
-  user.lastName = "Saw"
-  user.age = 25
-  await appDataSource.manager.save(user)
-  console.log("Saved a new user with id: " + user.id)
+  console.log("Inserting a new user into the database...");
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+  await appDataSource.manager.save(user);
+  console.log("Saved a new user with id: " + user.id);
 
-  console.log("Loading users from the database...")
-  const users = await appDataSource.manager.find(User)
-  console.log("Loaded users: ", users)
+  console.log("Loading users from the database...");
+  const users = await appDataSource.manager.find(User);
+  console.log("Loaded users: ", users);
 
-  console.log("You can setup and run express / fastify / any other framework.")
+  console.log("You can setup and run express / fastify / any other framework.");
 }
 main();
--- src/stark-db.ts
+++ src/stark-db.ts
@@ -1,6 +1,22 @@
-import 'reflect-metadata';
+// import 'reflect-metadata';
 import { getAppDataSource } from './browser/data-source';
+import { User } from "./entity/User";
 
-export {
-  getAppDataSource
+export async function main() {
+  const appDataSource = await getAppDataSource();
+  await appDataSource.initialize();
+
+  console.log("Inserting a new user into the database...");
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+  await appDataSource.manager.save(user);
+  console.log("Saved a new user with id: " + user.id);
+
+  console.log("Loading users from the database...");
+  const users = await appDataSource.manager.find(User);
+  console.log("Loaded users: ", users);
+
+  console.log("You can setup and run express / fastify / any other framework.");
 }
