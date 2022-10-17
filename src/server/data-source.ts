6b1abccef611965db5342f091e55b8af5c96c021
Adrian Burlacu <adrian.burlacu@live.com>
2022-10-17 13:30:29 +0300

    Add URI and flags to the sqlite3 driver.

--- src/server/data-source.ts
+++ src/server/data-source.ts
@@ -2,11 +2,23 @@ import 'reflect-metadata';
 
 import { DataSource } from 'typeorm';
 import { User } from '../entity/User';
+import sqlite3 from 'sqlite3';
 
 export async function getAppDataSource(): Promise<DataSource> {
+  // return new DataSource({
+  //   type: "sqlite",
+  //   database: "./test.db",
+  //   cache: true,
+  //   synchronize: true, // TODO: remove this in production
+  //   logging: false,
+  //   entities: [User],
+  //   migrations: [],
+  //   subscribers: [],
+  // });
   return new DataSource({
     type: "sqlite",
-    database: "./test.db",
+    database: "file:./test/tmp/test_memory.db?mode=memory",
+    flags: sqlite3.OPEN_URI | sqlite3.OPEN_SHAREDCACHE | sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     cache: true,
     synchronize: true, // TODO: remove this in production
     logging: false,
