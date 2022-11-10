d88c4570bc81cae1a154a3fe4d9aece1ffc31685
Adrian Burlacu <adrian.burlacu@live.com>
2022-11-10 11:54:59 +0200

    Add the UserDB tests.

--- test/server/DBUser.test.ts
+++ test/server/DBUser.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { existsSync, rmSync } from 'fs';
+import { resolve } from 'path';
 
 import { StarkVariable } from '../../src/entity/variable';
 import { UserDB } from '../../src/server/objects/DBUser';
@@ -8,24 +9,51 @@ const DB_PATH = `./test`;
 const DB_FILE = 'test.db';
 
 describe('User DB.', function () {
-  this.timeout(600e3);
+  // this.timeout(600e3);
 
   this.beforeAll(async () => {
+    const DBPath = resolve(DB_PATH, DB_FILE);
     // Delete the database file if it exists.
-    if (existsSync(`${DB_PATH}/${DB_FILE}`)) {
-      rmSync(`${DB_PATH}/${DB_FILE}`);
+    if (existsSync(DBPath)) {
+      rmSync(DBPath);
     }
+
+    // Set the number of variables that are created.
+    global.numVars = 0;
   });
 
-  it(`Hello world.`, async () => {
-    const adminDB = new UserDB({
+  it(`Inits DB.`, async () => {
+    const userDB = new UserDB({
       name: DB_FILE,
       path: DB_PATH,
       entities: [StarkVariable]
     });
 
-    await adminDB.validator.readyAsync();
+    await userDB.validator.readyAsync();
 
-    expect(true).to.be.equal(true);
+    const vars = await userDB
+      .DB
+      .manager
+      .find(StarkVariable);
+    
+    expect(vars.length).to.be.greaterThan(1);
+    global.numVars = vars.length;
+  });
+
+  it(`Avoids double initialization.`, async () => {
+    const userDB = new UserDB({
+      name: DB_FILE,
+      path: DB_PATH,
+      entities: [StarkVariable]
+    });
+
+    await userDB.validator.readyAsync();
+
+    const vars = await userDB
+      .DB
+      .manager
+      .find(StarkVariable);
+    
+    expect(vars.length).to.be.equal(global.numVars);
   });
 });
