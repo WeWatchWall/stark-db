20585367e4267b40196ddec10fc4f4f83d796dad
Adrian Burlacu <adrian.burlacu@live.com>
2023-10-10 13:37:41 +0300

    Remove dead package.

--- src/deps.d.ts
+++ src/deps.d.ts
@@ -1,5 +1,4 @@
 declare module 'sticky-session-custom';
-declare module 'project-version';
 declare module 'recursive-iterator';
 declare module '@appland/sql-parser';
 declare module 'flat-promise';
